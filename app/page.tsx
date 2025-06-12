bcca64d9b8b2ad0cc46e69658a751fd85604b2aa
SCharan1134 <132429792+SCharan1134@users.noreply.github.com>
2025-06-12 20:15:24 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -4,14 +4,15 @@ import PortfolioGrid from "@/components/portfolio-grid";
 import { getPortfolioItems } from "@/lib/data";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
+import VideoRow from "@/components/video-row";
 
 export default async function Home() {
   const portfolioItems = await getPortfolioItems();
 
   return (
-    <div className="bg-white">
+    <div className="bg-white overflow-hidden">
       <Header />
-      <main className="container mx-auto px-4 py-8 ">
+      <main className="px-4 py-8 ">
         <h1 className="sr-only">Portfolio Gallery</h1>
 
         <Suspense
@@ -28,6 +29,14 @@ export default async function Home() {
           }
         >
           <PortfolioGrid items={portfolioItems} />
+        </Suspense>
+
+        <Suspense
+          fallback={
+            <div className="text-center py-20">Loading portfolio items...</div>
+          }
+        >
+          <VideoRow />
         </Suspense>
       </main>
       <Footer />
